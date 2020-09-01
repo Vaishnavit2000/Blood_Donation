29e30596480e82271fa133b842a048804a0480e4
Vaishnavit2000 <45139610+Vaishnavit2000@users.noreply.github.com>
2020-09-01 21:16:41 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,19 @@
 <?php
 
+//Local data base
+// $user = 'Vaishnavi';
+// $password = 'cake_14';
+// $host ='localhost';
+// $db = 'blood_donar';
+
+//Remote database
+$user = 'aADLB8x8gl';
+$password = '6vEovCjtsv';
+$host ='remotemysql.com';
+$db = 'aADLB8x8gl';
+
 //My sql connection
-$conn = mysqli_connect('localhost' , 'Vaishnavi' , 'cake_14' , 'blood_donar');
+$conn = mysqli_connect($host, $user , $password , $db);
 
 //check for any connection error
 if(!$conn){
